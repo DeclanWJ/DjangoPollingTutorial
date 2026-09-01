8e6b866290a42230336f6f3dfe246f556090b004
Declan Jeffrey <declanj2000@hotmail.com>
2026-09-01 15:15:19 -0500

    feat(polling): adding in custom views, models, urls, and templates.

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("Polling index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request,"polls/index.html", context)
+
+# note, just test, if issue, change to % question_id
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question":question})
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except: Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, "polls/detail.html", {"quesiton":question})
+def results(request, question_id):
+    response = "You're looking at the results of question %s"
+    return HttpResponse(response % question_id)\
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
 # Create your views here.
